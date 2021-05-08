6989b3a001a45df0bb541b9bde915232b1359040
CodeDiary18 <codediary18@gmail.com>
2021-05-08 17:18:53 +0900

    내비게이션 바와 푸터 모듈화

--- django 웹사이트/blog/tests.py
+++ django 웹사이트/blog/tests.py
@@ -5,6 +5,24 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        # 내비게이션 바가 있다.
+        navbar = soup.nav
+        # Blog, About Me라는 문구가 내비게이션 바에 있다
+        self.assertIn('Blog',navbar.text)
+        self.assertIn('About Me',navbar.text)
+        logo_btn=navbar.find('a', text="Do it! Django")
+        self.assertEqual(logo_btn.attrs['href'],'/')
+        home_btn=navbar.find('a', text="Home")
+        self.assertEqual(home_btn.attrs['href'], '/')
+        blog_btn = navbar.find('a',text='Blog')
+        self.assertEqual(blog_btn.attrs['href'],"/blog/")
+        about_me_btn=navbar.find('a',text="About Me")
+        self.assertEqual(about_me_btn.attrs['href'],"/about_me/")
+
+
+
+
     def test_post_list(self)   :
         # 1.1 포스트 목록 페이지를 가져온다
         response = self.client.get('/blog/')
@@ -13,11 +31,8 @@ class TestView(TestCase):
         # 1.3 페이지 타이틀은 'Blog'이다.
         soup=BeautifulSoup(response.content,'html.parser')
         self.assertEqual(soup.title.text,'Blog')
-        # 1.4 내비게이션 바가 있다.
-        navbar = soup.nav
-        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다
-        self.assertIn('Blog',navbar.text)
-        self.assertIn('About Me',navbar.text)
+        # 1.4 내비게이션 바 검사
+        self.navbar_test(soup)
 
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(),0)
@@ -62,9 +77,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup=BeautifulSoup(response.content, 'html.parser')
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다
-        navbar=soup.nav
-        self.assertIn('Blog',navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
         # 2.3 첫번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어있다
         self.assertIn(post_001.title, soup.title.text)
         # 2.4 첫번째 포스트의 제목이 포스트 영역에 있다
