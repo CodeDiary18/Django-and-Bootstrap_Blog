511d2cdbd2fa3fd3c323fe4590cef1ed77eb6fc0
CodeDiary18 <codediary18@gmail.com>
2021-05-09 02:48:17 +0900

    django-crispy-forms 적용

--- django 웹사이트/project/settings.py
+++ django 웹사이트/project/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'crispy_forms',
     'blog',
     'single_pages',
 ]
@@ -113,6 +114,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
+CRISPY_TEMPLATE_PACK='bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
