d733fc1a857e171296c3e5fdc854d27310fd0d0f
CodeDiary18 <codediary18@gmail.com>
2021-05-08 15:17:18 +0900

    미리보기와 요약문 추가

--- django 웹사이트/blog/models.py
+++ django 웹사이트/blog/models.py
@@ -3,6 +3,7 @@ import os
 
 class Post(models.Model):   # Post Model 만들기
     title = models.CharField(max_length=30) #제목
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()            #내용
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
