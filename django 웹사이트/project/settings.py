bb84ef204f410948b358fc83ea2eee95c8c78480
CodeDiary18 <codediary18@gmail.com>
2021-05-09 04:14:10 +0900

    구글 로그인 기능 추가

--- django 웹사이트/project/settings.py
+++ django 웹사이트/project/settings.py
@@ -29,6 +29,11 @@ INSTALLED_APPS = [
     'django_extensions',
     'crispy_forms',
     'markdownx',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',#구글 로그인 사용
     'blog',
     'single_pages',
 ]
@@ -120,3 +125,18 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
