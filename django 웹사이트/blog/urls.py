20a522daa4bef40bf8794560813ac26d4022e355
CodeDiary18 <codediary18@gmail.com>
2021-05-08 21:03:46 +0900

    태그 페이지 만들기

--- django 웹사이트/blog/urls.py
+++ django 웹사이트/blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     #path('<int:pk>/',views.single_post_page),
     #<int:pk>는 정수 형태의 값을 pk라는 변수로 담아 single_post_page() 함수로 넘기라는 의미
     #path('',views.index),
+    path('tag/<str:slug>/',views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/',views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
--- django 웹사이트/blog/views.py
+++ django 웹사이트/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class PostList(ListView):#기존 FBV의 index와 같은 역할
     model=Post  # 포스트 목록
@@ -38,6 +38,20 @@ def category_page(request, slug):
             'categories':Category.objects.all(),
             'no_category_post_count':Post.objects.filter(category=None).count(),
             'category':category,
+        }
+    )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list=tag.post_set.all
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list':post_list,
+            'tag':tag,
+            'categories':Category.objects.all(),
+            'no_category_post_count':Post.objects.filter(category=None).count(),
         }
     )
 # def index(request):
