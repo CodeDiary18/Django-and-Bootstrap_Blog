ad4e9024b0b32f8e30a778bafdacd60959522acd
CodeDiary18 <codediary18@gmail.com>
2021-05-09 20:03:01 +0900

    사용자 아바타 보여주기

--- django 웹사이트/blog/models.py
+++ django 웹사이트/blog/models.py
@@ -61,6 +61,11 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f"https://doitdjango.com/avatar/id/131/83a0315115dc3c15/svg/{self.author.email}"
 
     def __str__(self):
         return f'{self.author}::{self.content}'
