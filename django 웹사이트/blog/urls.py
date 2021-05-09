57fbc1165bee6caa4ade9835657528e94be74cd5
CodeDiary18 <codediary18@gmail.com>
2021-05-09 19:12:29 +0900

    댓글 삭제 기능 추가하기

--- django 웹사이트/blog/urls.py
+++ django 웹사이트/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     #<int:pk>는 정수 형태의 값을 pk라는 변수로 담아 single_post_page() 함수로 넘기라는 의미
     #path('',views.index),
     path('update_comment/<int:pk>/',views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/',views.delete_comment),
     path('update_post/<int:pk>/',views.PostUpdate.as_view()),
     path('create_post/',views.PostCreate.as_view()),
     path('tag/<str:slug>/',views.tag_page),
