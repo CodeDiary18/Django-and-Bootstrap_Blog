e5cb7cc8ea88d59d0190b5cde5c0587c2f6448e1
CodeDiary18 <codediary18@gmail.com>
2021-05-09 22:46:12 +0900

    대문 페이지 수정

--- django 웹사이트/blog/models.py
+++ django 웹사이트/blog/models.py
@@ -55,6 +55,12 @@ class Post(models.Model):   # Post Model 만들기
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f"https://doitdjango.com/avatar/id/131/83a0315115dc3c15/svg/{self.author.email}"
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
